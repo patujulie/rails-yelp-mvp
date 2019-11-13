05cbf21ef634d73eaf74c7aa9f525b5167c46198
Julie Paturel <patujulie@gmail.com>
2019-11-13 15:19:04 +0100

    rails MVP routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+  # get "restaurants/:id", to: "restaurants#show"
+  # get    'restaurants',          to: 'restaurants#index', as: :restaurants
+
+  # post   'restaurants',          to: 'restaurants#create'
+
+  # get    'restaurants/:id',      to: 'restaurants#show',   as: :restaurant
+
+  # get    'restaurants/:id/edit', to: 'restaurants#edit',   as: :edit_restaurant
+  # patch  'restaurants/:id',      to: 'restaurants#update'
+
+  # delete 'restaurants/:id',      to: 'restaurants#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     "#{CATEGORIES.sample}",
+    phone_number:  "0637712909"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  "#{CATEGORIES.sample}",
+    phone_number:  '0632472912'
+  },
+  {
+    name:         'Beerstro',
+    address:      '62A rue du pont neuf ',
+    category:  "#{CATEGORIES.sample}",
+    phone_number:  '0632545552'
+  },
+  {
+    name:         'bouillon du tarn',
+    address:      'avenue laurent hitch ',
+    category:  "#{CATEGORIES.sample}",
+    phone_number:  '021585552'
+  },
+  {
+    name:         'assiete creuse',
+    address:      'rue des moche  ',
+    category:  "#{CATEGORIES.sample}",
+    phone_number:  '025441552'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
